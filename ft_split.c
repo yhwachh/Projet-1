a3eb72c1000d60379ac40c0a01a6c72297b94b63
Ibraguim Albakov <ibalbako@k2r4p9.42mulhouse.fr>
2022-03-09 12:53:32 +0100

    12 commit

--- ft_split.c
+++ ft_split.c
@@ -11,99 +11,6 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-# include <stdlib.h>
-# include <unistd.h>
-# include <string.h>
-/*static int	count_w(char const *s, char c)
-{
-	int	i;
-	int	nb;
-	int	words;
-
-	i = 0;
-	nb = 0;
-	if (!s)
-		return (-1);
-	while (s[i] != '\0')
-	{
-		words = 0;
-		while ((s[i] == c) && s[i] != '\0')
-			i++;
-		while (s[i] != c && s[i] != '\0')
-		{
-			i++;
-			words = 1;
-		}
-		if (words == 1)
-			nb++;
-	}
-	return (nb);
-}
-
-static int	count_l(char const *s, char c)
-{
-	int	i;
-
-	i = 0;
-	while (s[i] && !(s[i] == c))
-		i++;
-	return (i);
-}
-
-char	**ft_strsplit(char const *s, char c)
-{
-	char	**tab;
-	int		i;
-	int		count;
-	int		word_len;
-	int		nb_of_words;
-
-	count = -1;
-	if ((nb_of_words = count_w(s, c)) == -1)
-		return (NULL);
-	tab = (char **)malloc(sizeof(*tab) * count_w(s, c) + 1);
-	if (!tab)
-		return (NULL);
-	while (++count < nb_of_words)
-	{
-		while ((*s == c) && *s)
-			s++;
-		word_len = count_l(s, c);
-		tab[count] = (char *)malloc(word_len + 1);
-		if (!tab)
-			return (NULL);
-		tab[count][word_len] = '\0';
-		i = 0;
-		while (i < word_len)
-			tab[count][i++] = *s++;
-	}
-	tab[count] = 0;
-	return (tab);
-}
-*/
-
-char	*ft_substr(char const *s, unsigned int start, size_t len)
-{
-	char	*mot;
-	int		i;
-
-	i = 0;
-	if (!s)
-		return (NULL);
-	mot = malloc((len + 1) * sizeof(char));
-	if (!mot)
-		return (NULL);
-	mot[len] = '\0';
-	while (len--)
-	{
-		mot[i] = s[start];
-		i++;
-		start++;
-	}
-	return (mot);
-}
-
-#include <stdio.h>
 
 static size_t	ft_facul(const char *s, char c)
 {
@@ -150,17 +57,4 @@ char	**ft_split(const char *s, char c)
 	}
 	ret[i] = 0;
 	return (ret);
-}
-int main() {
-
-	char s[] = "hell$g$$$gg&&222$jj";
-	char c = '$';
-	char **tab = ft_split(s,c);
-
-	for(int i = 0; i < 3 ; i++ )
-	{
-		printf("%s", tab[i]);
-
-	}
-	
 }
